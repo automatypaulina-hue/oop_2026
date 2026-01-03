9b2502289d4753c711c4361fdd60afb6f46de7fc
automatypaulina-hue <automatypaulina@gmail.com>
2026-01-03 11:28:00 +0100

    add czlowiek

--- klasy.py
+++ klasy.py
@@ -2,12 +2,16 @@
 class Czlowiek:
     #istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
-        print("Niech powstanie czlowiek")
+    def __init__(self, imie):
+        print(f"Niech powstanie czlowiek o imieniu {imie}")
+        self.imie = imie
+        # adam.imie = "Adam"
+        # ewa.imie = "Ewa"
 #Powstanie obiektu
 #Gotowanie z przepisu
-adam = Czlowiek()
-#print(type(adam))
-#print(dir(Czlowiek))
-#print(dir(adam))
+adam = Czlowiek("Adam")
+ewa = Czlowiek('Ewa')
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
