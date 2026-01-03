db42b21a1c74504ac6df832474415a8f56c80a7d
automatypaulina-hue <automatypaulina@gmail.com>
2026-01-03 13:39:30 +0100

    Add baw_sie

--- klasy.py
+++ klasy.py
@@ -1,17 +1,52 @@
-#klasa = szablon , przepis
+#Klasa = Szablon, Przepis
+from tokenize import String
+
+
 class Czlowiek:
-    #istota
-    gatunek = "Homo Sapiens"
-    def __init__(self, imie):
+    #Istota
+    #Atrybuty klasy
+    gatunek = "Homo sapiens"
+    def __init__(self, imie, plec): #Atrybuty OOBIEKTU (skladniki potrawy)
+        # Konstruktor
+        # Akt istnienia
         print(f"Niech powstanie czlowiek o imieniu {imie}")
         self.imie = imie
-        # adam.imie = "Adam"
-        # ewa.imie = "Ewa"
-#Powstanie obiektu
+        self.plec = plec
+        #adam.imie = "Adam"
+        #ewa.imie = "Ewa"
+
+    #Metoda
+    #Moznosc (mozliwosc), zdolnosc, umiejetnosc
+    def przedstaw_sie(self):
+        print(f"Dzien dobry, am na imie {self.imie} i jestem", end="")
+        if self.plec == "M":
+            print("mezczyzna")
+        else:
+            print("kobieta")
+
+    def przedstaw(self, osoba):
+        print(f"Oto {osoba.imie}")
+class Dziecko(Czlowiek):
+    def baw_sie(self):
+        print("Ale zabawa, juhuuu!!!!!")
+    def przedstaw_sie(self):
+        print(f"Cesc, jestem {self.imie} i jestem", end="")
+        if self.plec == "M":
+            print("chlopcem")
+        else:
+            print("dziewczynka")
+
+#Powstanie z obiektu
 #Gotowanie z przepisu
-adam = Czlowiek("Adam")
-ewa = Czlowiek('Ewa')
+
+adam = Czlowiek("Adam", "M")
+ewa = Czlowiek("Ewa", "K")
+kain = Dziecko("Kain", "M")
+kain.baw_sie()
+
+
 print(adam.gatunek)
 print(ewa.gatunek)
-print(adam.imie)
-print(ewa.imie)
+
+adam.przedstaw_sie()
+ewa.przedstaw(adam)
